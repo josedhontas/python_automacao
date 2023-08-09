32adb3721c6aebccb28a669fcb8ad576a7997b8e
josedhonatas <dhonatas@academico.ufs.br>
2023-08-09 00:08:54 -0300

    quase_pronto

--- ocorrecias_periodo.py
+++ ocorrecias_periodo.py
@@ -1,24 +1,29 @@
 import json
+from unidecode import unidecode
 
-def atualizar_periodo_disciplina(nome_disciplina, nome_curso, novo_periodo):
-    # Abre o arquivo JSON com a codificação ISO-8859-1 (latin1)
-    with open('dados_modificados.json', 'r', encoding='iso-8859-1') as json_file:
-        data = json.load(json_file)
+def atualizar_periodo_por_nome_materia(dados, nome_materia, novo_periodo):
+    for periodo in dados:
+        for disciplina in dados[periodo]:
+            nome_disciplina = (dados[periodo][disciplina]['nome'])
+            nome_materia_formatado = (nome_materia)
+            print(nome_disciplina, nome_materia_formatado)
+            if nome_disciplina == nome_materia_formatado:
+                print(nome_disciplina)
+                dados[periodo][disciplina]['periodo'] = novo_periodo
 
-    if nome_curso in data and nome_disciplina in data[nome_curso]:
-        # Atualiza o atributo "periodo" para a disciplina especificada
-        data[nome_curso][nome_disciplina]['periodo'] = novo_periodo
+# Abre o arquivo JSON com a codificação ISO-8859-1 (latin1)
+with open('dados_modificados.json', 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
 
-        # Escreve o JSON modificado de volta ao arquivo
-        with open('dados.json', 'w', encoding='iso-8859-1') as json_file:
-            json.dump(data, json_file, ensure_ascii=False, indent=4)
+# Parâmetros para atualização
+nome_materia = "Programação Imperativa"  # Nome da matéria a ser atualizada
+novo_periodo = "2°"  # Novo valor para o atributo "periodo"
 
-        print(f"Atributo 'periodo' da disciplina '{nome_disciplina}' do curso '{nome_curso}' atualizado para '{novo_periodo}'.")
-    else:
-        print(f"Disciplina '{nome_disciplina}' não encontrada no curso '{nome_curso}'.")
+# Chama a função para atualizar o valor do atributo "periodo"
+atualizar_periodo_por_nome_materia(data['cc'], nome_materia, novo_periodo)
 
-# Chame a função com os valores desejados
-nome_disciplina = "Programação Imperativa"
-nome_curso = "cc"
-novo_periodo = "2°"
-atualizar_periodo_disciplina(nome_disciplina, nome_curso, novo_periodo)
+# Escreve o JSON modificado de volta no arquivo
+with open('dados_modificados.json', 'w', encoding='utf-8') as json_file:
+    json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+print(f"Atributo 'periodo' atualizado para '{novo_periodo}' para a matéria '{nome_materia}' em todos os períodos.")
